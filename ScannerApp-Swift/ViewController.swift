52defef27e1a031c6b835812cb93f7009cf9fe12
Justin Xiao <jx16dev@gmail.com>
2019-12-20 13:17:44 -0800

    First commit.

---
+++ ScannerApp-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScannerApp-Swift
+//
+//  Created by Zheren Xiao on 2019-12-20.
+//  Copyright © 2019 jx16. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
