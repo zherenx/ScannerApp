55bec3471f93011ecb35ac1825dd0a896fa8dc88
Justin Xiao <jx16dev@gmail.com>
2019-12-20 23:38:24 -0800

    Add CameraViewController.

---
+++ ScannerApp-Swift/CameraViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CameraViewController.swift
+//  ScannerApp-Swift
+//
+//  Created by Zheren Xiao on 2019-12-20.
+//  Copyright © 2019 jx16. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
