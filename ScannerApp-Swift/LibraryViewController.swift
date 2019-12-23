716e2e6838836759f1d9b23645feba85707a0690
Justin Xiao <jx16dev@gmail.com>
2019-12-23 00:56:08 -0800

    Add LibraryViewController.

---
+++ ScannerApp-Swift/LibraryViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LibraryViewController.swift
+//  ScannerApp-Swift
+//
+//  Created by Zheren Xiao on 2019-12-23.
+//  Copyright © 2019 jx16. All rights reserved.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true))
+        
+        let fileManager = FileManager.default
+        let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        do {
+            let fileURLs = try fileManager.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil)
+            
+            print(fileURLs)
+            
+        } catch {
+            print("Error while enumerating files \(documentsURL.path): \(error.localizedDescription)")
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
