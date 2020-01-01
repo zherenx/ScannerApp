1f8ee1320c949db9c95675a42bb185555d74933e
Justin Xiao <jx16dev@gmail.com>
2019-12-31 19:56:52 -0800

    Update table view cell layout, update upload routine.

--- ScannerApp-Swift/Model/HttpRequestHandler.swift
+++ ScannerApp-Swift/Model/HttpRequestHandler.swift
@@ -10,15 +10,17 @@ import Foundation
 
 protocol HttpRequestHandlerDelegate {
     func didReceiveUploadProgressUpdate(progress: Float)
+    func didCompleteUpload(data: Data?,response: URLResponse?, error: Error?)
 }
 
 class HttpRequestHandler: NSObject {
     
-    let host = URL(string: "http://192.168.1.69:5000/upload")!
+    private let host = URL(string: "http://192.168.1.69:5000/upload")!
+    private let uploadQueue = OperationQueue()
     var httpRequestHandlerDelegate: HttpRequestHandlerDelegate?
     
     // TODO: return type should be whatever http response code type is
-    func upload(toUpload fileUrl: URL) -> Int {
+    func upload(toUpload fileUrl: URL) {
         
         var request = URLRequest(url: host)
         request.allowsCellularAccess = false
@@ -30,35 +32,35 @@ class HttpRequestHandler: NSObject {
 //        var config = URLSessionConfiguration.background(withIdentifier: "url session")
 //        let session = URLSession(configuration: config, delegate: self, delegateQueue: nil)
         
-        let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
-        let task = session.uploadTask(with: request, fromFile: fileUrl)
-
-        
-//        let task = URLSession.shared.uploadTask(with: request, fromFile: fileUrl) { data, response, error in
-//            if let error = error {
-//                print ("error: \(error)")
-//                return
-//            }
-//            guard let response = response as? HTTPURLResponse,
-//                (200...299).contains(response.statusCode) else {
-//                print ("server error")
-//                return
-//            }
-//            if let mimeType = response.mimeType,
-//                mimeType == "application/json",
-//                let data = data,
-//                let dataString = String(data: data, encoding: .utf8) {
-//                print ("got data: \(dataString)")
-//            }
-//        }
-        
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: uploadQueue)
+        let task = session.uploadTask(with: request, fromFile: fileUrl, completionHandler: {
+            data, response, error in
+            
+            if let delegate = self.httpRequestHandlerDelegate {
+                delegate.didCompleteUpload(data: data, response: response, error: error)
+            } else {
+                print("Upload complete")
+                
+                if let error = error {
+                    print ("error: \(error)")
+                    return
+                }
+                guard let response = response as? HTTPURLResponse,
+                    (200...299).contains(response.statusCode) else {
+                    print ("server error")
+                    return
+                }
+                if let mimeType = response.mimeType,
+                    mimeType == "application/json",
+                    let data = data,
+                    let dataString = String(data: data, encoding: .utf8) {
+                    print ("got data: \(dataString)")
+                }
+            }
+            
+        })
 
         task.resume()
-        
-        
-        
-        // TODO: return status
-        return 0
     }
 }
 
--- ScannerApp-Swift/View/ScanTableViewCell.swift
+++ ScannerApp-Swift/View/ScanTableViewCell.swift
@@ -19,6 +19,9 @@ class ScanTableViewCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var uploadProgressView: UIProgressView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -38,12 +41,17 @@ class ScanTableViewCell: UITableViewCell {
         
         self.infoLabel.text = url.pathExtension
         self.infoLabel.textColor = .darkGray
+        
+        self.uploadProgressView.isHidden = true
     }
     
     // TODO: behavior related stuff probably should be in a controller class
     @IBAction func uploadButtonTapped(_ sender: Any) {
-        // TODO:
-        // Setup UI
+        DispatchQueue.main.async {
+            self.uploadButton.isEnabled = false
+            self.deleteButton.isEnabled = false
+            self.uploadProgressView.isHidden = false
+        }
         
         let requestHandler = HttpRequestHandler()
         requestHandler.httpRequestHandlerDelegate = self
@@ -62,7 +70,38 @@ class ScanTableViewCell: UITableViewCell {
 }
 
 extension ScanTableViewCell: HttpRequestHandlerDelegate {
+    
     func didReceiveUploadProgressUpdate(progress: Float) {
-        print(progress)
+//        print(progress)
+        DispatchQueue.main.async {
+            self.uploadProgressView.progress = progress
+        }
+    }
+    
+    func didCompleteUpload(data: Data?, response: URLResponse?, error: Error?) {
+
+        DispatchQueue.main.async {
+            self.uploadButton.isEnabled = true
+            self.deleteButton.isEnabled = true
+            self.uploadProgressView.isHidden = true
+        }
+        
+        print("test code")
+        
+        if let error = error {
+            print ("error: \(error)")
+            return
+        }
+        guard let response = response as? HTTPURLResponse,
+            (200...299).contains(response.statusCode) else {
+                print ("server error")
+                return
+        }
+        if let mimeType = response.mimeType,
+            mimeType == "application/json",
+            let data = data,
+            let dataString = String(data: data, encoding: .utf8) {
+            print ("got data: \(dataString)")
+        }
     }
 }
