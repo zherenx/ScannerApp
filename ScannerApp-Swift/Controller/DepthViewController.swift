72a098de35483626361d14066607d49f238c6c1e
Justin Xiao <jx16dev@gmail.com>
2020-01-25 13:54:44 -0800

    Add DepthViewController and UI for testing.

---
+++ ScannerApp-Swift/Controller/DepthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DepthViewController.swift
+//  ScannerApp-Swift
+//
+//  Created by Zheren Xiao on 2020-01-25.
+//  Copyright © 2020 jx16. All rights reserved.
+//
+
+import UIKit
+
+class DepthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
