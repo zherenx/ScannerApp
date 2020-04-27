fb794c31a0cc7e70cd10cee471b2533f2b6a73ff
Justin Xiao <jx16dev@gmail.com>
2020-04-26 19:10:14 -0700

    Update recording detail page.

--- ScannerApp-Swift/Controller/RecordingDetailViewController.swift
+++ ScannerApp-Swift/Controller/RecordingDetailViewController.swift
@@ -10,26 +10,57 @@ import UIKit
 
 class RecordingDetailViewController: UIViewController {
     
-    public var recordingUrl: URL?
+    var recordingUrl: URL?
 
-    @IBOutlet private weak var recordingUrlLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    @IBOutlet private weak var recordingIdLabel: UILabel!
+    
+    @IBOutlet weak var recordingDetailLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        populateView()
+    }
 
-        recordingUrlLabel.text = recordingUrl?.lastPathComponent
+    private func populateView() {
+        
+        if let recordingUrl = recordingUrl {
+            
+            recordingIdLabel.text = "Recording ID: \(recordingUrl.lastPathComponent)"
+            
+            if recordingUrl.hasDirectoryPath {
+                var fileURLs: [URL] = []
+                do {
+                    fileURLs = try FileManager.default.contentsOfDirectory(at: recordingUrl, includingPropertiesForKeys: nil)
+                } catch {
+                    print("Error while enumerating files \(recordingUrl.path): \(error.localizedDescription)")
+                }
+                
+                for fileUrl in fileURLs {
+                    let extention = fileUrl.pathExtension
+                    
+                    // TODO: if statement might work better here
+                    switch extention {
+                    case "mp4":
+                        let thumbnail = VideoHelper.generateThumbnail(videoUrl: fileUrl)
+                        thumbnailImageView.image = UIImage(cgImage: thumbnail)
+                    case "json":
+                        recordingDetailLabel.text = "recording detail ..."
+                    default:
+                        break
+                    }
+                }
+            } else {
+                // if url is a file
+                
+                // TODO: maybe this else block is not necessary
+                recordingDetailLabel.text = ""
+            }
+        } else {
+            print("Internal error!\nRecording url is not valid.")
+        }
         
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
