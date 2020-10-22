446bfa0168124cc1f4614df821f775a919276cdf
Justin Xiao <jx16dev@gmail.com>
2020-10-22 13:10:37 -0700

    Setup PopUpView layout.

--- ScannerApp-Swift/View/PopUpView.swift
+++ ScannerApp-Swift/View/PopUpView.swift
@@ -18,50 +18,85 @@ class PopUpView: UIView {
     private var sceneTypeIndex = 0
 //    private var sceneType: String?
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    init(firstName: String, lastName: String, description: String, sceneTypeIndex: Int) {
         
-        setupLayout()
-        initValues()
-    }
-    
-    init(frame: CGRect, firstName: String, lastName: String, description: String, sceneTypeIndex: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.userInputDescription = description
+        self.sceneTypeIndex = sceneTypeIndex
         
+//        let frame = CGRect(x: 0, y: 0, width: <#T##Int#>, height: <#T##Int#>)
+        super.init(frame: .zero)
+        
+//        self.backgroundColor = .white
+//        self.alpha = 1
+        
+        setupViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    let firstNameLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "First Name"
+        return lb
+    }()
+    
+    let lastNameLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "Last Name"
+        return lb
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "Description"
+        return lb
+    }()
+    
+//    let sceneTypeLabel: UILabel = {
+//        let lb = UILabel()
+//        lb.text = "Scene Type"
+//        return lb
+//    }()
+    
     let firstNameTextField: UITextField = {
         let tf = UITextField()
-        tf.placeholder = "Enter firstname"
+        tf.placeholder = "Enter first name"
+        tf.layer.borderColor = UIColor.lightGray.cgColor
+        tf.layer.borderWidth = 1
         return tf
     }()
     
     let lastNameTextField: UITextField = {
         let tf = UITextField()
-        tf.placeholder = "Enter lastname"
+        tf.placeholder = "Enter last name"
+        tf.layer.borderColor = UIColor.lightGray.cgColor
+        tf.layer.borderWidth = 1
         return tf
     }()
     
     let descriptionTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Enter scene description"
+        tf.layer.borderColor = UIColor.lightGray.cgColor
+        tf.layer.borderWidth = 1
         return tf
     }()
     
-    lazy var selectSceneTypeButton: UIButton = {
-        let btn = UIButton(type: .system)
-        btn.setTitle(sceneTypes[sceneTypeIndex], for: .normal)
-        return btn
-    }()
+//    lazy var selectSceneTypeButton: UIButton = {
+//        let btn = UIButton(type: .system)
+//        btn.setTitle(sceneTypes[sceneTypeIndex], for: .normal)
+//        return btn
+//    }()
     
     lazy var sceneTypePickerView: UIPickerView = {
         let pv = UIPickerView()
         pv.delegate = self
         pv.dataSource = self
-        pv.isHidden = true
+//        pv.isHidden = true
         pv.selectRow(sceneTypeIndex, inComponent: 0, animated: false)
         return pv
     }()
@@ -75,16 +110,87 @@ class PopUpView: UIView {
     let startButton: UIButton = {
         let btn = UIButton(type: .system)
         btn.setTitle("Start Recording", for: .normal)
+        btn.isEnabled = false
         return btn
     }()
     
-    func setupLayout() {
+    func setupViews() {
+        
+//        self.translatesAutoresizingMaskIntoConstraints = false
+        self.heightAnchor.constraint(equalToConstant: 420).isActive = true
+        self.widthAnchor.constraint(equalToConstant: 330).isActive = true
+        self.backgroundColor = UIColor(white: 1, alpha: 0.8)
+        
+        // first name row
+        addSubview(firstNameLabel)
+        firstNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        firstNameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        firstNameLabel.widthAnchor.constraint(equalToConstant: 90).isActive = true
+        firstNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        firstNameLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 8).isActive = true
+        
+        addSubview(firstNameTextField)
+        firstNameTextField.translatesAutoresizingMaskIntoConstraints = false
+        firstNameTextField.heightAnchor.constraint(equalTo: firstNameLabel.heightAnchor).isActive = true
+        firstNameTextField.topAnchor.constraint(equalTo: firstNameLabel.topAnchor).isActive = true
+        firstNameTextField.leftAnchor.constraint(equalTo: firstNameLabel.rightAnchor, constant: 8).isActive = true
+        firstNameTextField.rightAnchor.constraint(equalTo: rightAnchor, constant: -8).isActive = true
+
+        // last name row
+        addSubview(lastNameLabel)
+        lastNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        lastNameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        lastNameLabel.widthAnchor.constraint(equalToConstant: 90).isActive = true
+        lastNameLabel.topAnchor.constraint(equalTo: firstNameLabel.bottomAnchor, constant: 8).isActive = true
+        lastNameLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 8).isActive = true
+        
+        addSubview(lastNameTextField)
+        lastNameTextField.translatesAutoresizingMaskIntoConstraints = false
+        lastNameTextField.heightAnchor.constraint(equalTo: lastNameLabel.heightAnchor).isActive = true
+        lastNameTextField.topAnchor.constraint(equalTo: lastNameLabel.topAnchor).isActive = true
+        lastNameTextField.leftAnchor.constraint(equalTo: lastNameLabel.rightAnchor, constant: 8).isActive = true
+        lastNameTextField.rightAnchor.constraint(equalTo: rightAnchor, constant: -8).isActive = true
+        
+        // description row
+        addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        descriptionLabel.widthAnchor.constraint(equalToConstant: 90).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: lastNameLabel.bottomAnchor, constant: 8).isActive = true
+        descriptionLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 8).isActive = true
+
+        addSubview(descriptionTextField)
+        descriptionTextField.translatesAutoresizingMaskIntoConstraints = false
+        descriptionTextField.heightAnchor.constraint(equalTo: descriptionLabel.heightAnchor).isActive = true
+        descriptionTextField.topAnchor.constraint(equalTo: descriptionLabel.topAnchor).isActive = true
+        descriptionTextField.leftAnchor.constraint(equalTo: descriptionLabel.rightAnchor, constant: 8).isActive = true
+        descriptionTextField.rightAnchor.constraint(equalTo: rightAnchor, constant: -8).isActive = true
+        
+        addSubview(sceneTypePickerView)
+        sceneTypePickerView.translatesAutoresizingMaskIntoConstraints = false
+        sceneTypePickerView.heightAnchor.constraint(equalToConstant: 180).isActive = true
+        sceneTypePickerView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 8).isActive = true
+        sceneTypePickerView.leftAnchor.constraint(equalTo: leftAnchor, constant: 8).isActive = true
+        sceneTypePickerView.rightAnchor.constraint(equalTo: rightAnchor, constant: -8).isActive = true
+        
+        addSubview(cancelButton)
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        cancelButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        cancelButton.topAnchor.constraint(equalTo: sceneTypePickerView.bottomAnchor, constant: 8).isActive = true
+        cancelButton.leftAnchor.constraint(equalTo: leftAnchor, constant: 20).isActive = true
+        cancelButton.rightAnchor.constraint(equalTo: centerXAnchor, constant: -20).isActive = true
+        cancelButton.backgroundColor = .yellow
+        
+        addSubview(startButton)
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        startButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        startButton.topAnchor.constraint(equalTo: sceneTypePickerView.bottomAnchor, constant: 8).isActive = true
+        startButton.leftAnchor.constraint(equalTo: centerXAnchor, constant: 20).isActive = true
+        startButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -20).isActive = true
+        startButton.backgroundColor = .yellow
         
     }
     
-    func initValues() {
-        
-    }
 }
 
 extension PopUpView: UITextFieldDelegate {
@@ -138,7 +244,7 @@ extension PopUpView: UIPickerViewDelegate, UIPickerViewDataSource {
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         UserDefaults.set(sceneTypeIndex: row)
-        selectSceneTypeButton.setTitle(sceneTypes[row], for: .normal)
+//        selectSceneTypeButton.setTitle(sceneTypes[row], for: .normal)
         
 //        updateSceneType()
 //        updateStartButton()
