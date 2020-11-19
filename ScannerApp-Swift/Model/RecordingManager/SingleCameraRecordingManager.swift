c1008b0a5b7ee1d7e572495aeb98e889d19a4c39
Justin Xiao <jx16dev@gmail.com>
2020-11-18 21:05:37 -0800

    Add SingleCameraRecordingManager.

---
+++ ScannerApp-Swift/Model/RecordingManager/SingleCameraRecordingManager.swift
@@ -0,0 +1,9 @@
+//
+//  SingleCameraRecordingManager.swift
+//  ScannerApp-Swift
+//
+//  Created by Zheren Xiao on 2020-11-18.
+//  Copyright © 2020 jx16. All rights reserved.
+//
+
+import Foundation
