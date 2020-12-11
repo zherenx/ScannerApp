949648dc9e2084be97298326b461219ef8d5ed97
Justin Xiao <jx16dev@gmail.com>
2020-12-11 00:01:06 -0800

    Add DualCameraRecordingManager file.

---
+++ ScannerApp-Swift/Model/RecordingManager/DualCameraRecordingManager.swift
@@ -0,0 +1,9 @@
+//
+//  DualCameraRecordingManager.swift
+//  ScannerApp-Swift
+//
+//  Created by Zheren Xiao on 2020-12-11.
+//  Copyright © 2020 jx16. All rights reserved.
+//
+
+import Foundation
